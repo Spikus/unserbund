014f1b284f3db3afbe1ed4e31eb01707c132752b
Danyl Yehorov <dyehorov@gmail.com>
2020-12-08 21:55:16 +0300

    home fix

--- wp-content/themes/unserbund2020/page-home.php
+++ wp-content/themes/unserbund2020/page-home.php
@@ -15,35 +15,37 @@ get_header();
                     $new_query->the_post() ?>
                 <article class="rotNew">
                     <h3 class="rotNew_title">
-                    <?php the_title(); ?>
+                        <?php the_title(); ?>
                     </h3>
                     <div class="col-md-3">
                         <figure class="newThumb">
-                             <?php the_post_thumbnail('medium'); ?>
-                         </figure>
+                            <?php the_post_thumbnail('medium'); ?>
+                        </figure>
                     </div>
                     <div class="col-md-9">
                         <ul class="nav nav-tabs nav-justified" id="mediaTabs">
                             <li class="active"><a href="#info<?php the_ID(); ?>" data-toggle="tab">Информация</a></li>
                             <li><a href="#pedigree<?php the_ID(); ?>" data-toggle="tab">Родословная</a></li>
-                            <li><a href="#photos<?php the_ID(); ?>" data-toggle="tab">Фото</a></li>
                         </ul>
                         <div class="tab-content">
                             <div class="tab-pane active info" id="info<?php the_ID(); ?>">
                                 <?php the_content() ?>
+                                <div class="photos">
+                                    <?php $var_gallery = get_field('album') ?>
+                                    <?php foreach($var_gallery as $img): ?>
+                                    <a rel="lightbox[roadtrip]" href="<?php echo $img["sizes"]["large"] ?>"
+                                        rel="lightbox" data-link="#">
+                                        <img class="img-thumbnail" src="<?php echo $img["sizes"]["thumbnail"] ?>"
+                                            alt="ротвейлер <?php get_the_title() ?>">
+                                    </a>
+                                    <?php endforeach; ?>
+                                </div>
+
                             </div>
                             <div class="tab-pane pedigree" id="pedigree<?php the_ID(); ?>">
                                 <?php echo get_field('first') ?>
                             </div>
-                            <div class="tab-pane photos" id="photos<?php the_ID(); ?>">
-                                <?php $var_gallery = get_field('album') ?>
-                                <?php foreach($var_gallery as $img): ?>
-                                    <a rel="lightbox[roadtrip]" href="<?php echo $img["sizes"]["large"] ?>" rel="lightbox" data-link="#" >
-                                        <img class="img-thumbnail" src="<?php echo $img["sizes"]["thumbnail"] ?>" alt="ротвейлер <?php get_the_title() ?>">
-                                    </a>      			
-                                <?php endforeach; ?>
-                            </div>
-                    </div>
+                        </div>
                 </article>
                 <?php endwhile; ?>
                 <?php while (have_posts()) : the_post(); ?>
